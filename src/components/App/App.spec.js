ce74bca7defe3045818fb0b0bc52780935bdd05f
Hannah Werman <hanna.w28@gmail.com>
2018-08-18 15:18:17 -0400

    create app and app tests, wrote calculator tests

---
+++ src/components/App/App.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import Calculator from '../Calculator/Calculator';
+
+describe('App', () => {
+  it('should render a <div />', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('div').length).toEqual(1);
+  });
+
+  it('should render the Calculator Component', () => {
+    expect(wrapper.containsMatchingElement(<Calculator />)).toEqual(true);
+  });
+});

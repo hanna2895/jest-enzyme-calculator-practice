9eb4e4d4f03ca89f9dc487b5e563355284322ecb
Hannah Werman <hanna.w28@gmail.com>
2018-08-18 18:11:28 -0400

    refactor display, add snapshot testing

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = ({displayValue}) => <div className="display-container" />;
+const Display = ({displayValue}) => {
+  return (
+    <div className="display-container">
+      <p className="display-value">
+        {displayValue}
+      </p>
+    </div>
+  );
+}
 
 Display.propTypes = { displayValue: PropTypes.string.isRequired};
 
